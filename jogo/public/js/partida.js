334123fbc942a1e8fd3c6d3cd40b6dbaedeaf8f2
Lucaslperes <49460069+Lucaslperes@users.noreply.github.com>
2019-12-02 21:29:15 -0300

    update partida.js

--- jogo/public/js/partida.js
+++ jogo/public/js/partida.js
@@ -1,17 +1,33 @@
 class partida {
-	var pedidoTruco = "";
-	var pedidoEnvido = "";
-	var pedidoFlor = "";
-	var cartas = [];
 	constructor(){
+		this.pedidoTruco = "";
+	    this.pedidoEnvido = "";
+	    this.pedidoFlor = "";
+	    this.cartas = {};/*
+	    {
+	    jogaUm: {
+			cartas: {
+				cartaUm: '',
+				cartaDois: '',
+				cartaTres: ''
+			}
+		},
+		jogaDois: {
+			cartas: {
+				cartaUm: '',
+				cartaDois: '',
+				cartaTres: ''
+			}
+		}
+	};*/
 		this.socket = io.connect('http://localhost');
 		this.timeout = document.getElementById("timeout");
 
 		this.labelEmbaralha = document.getElementById("labelEmbaralhar");
 		
 		this.botaoEnviar = document.getElementById("enviar"); 
-		
-		
+		this.botaoEmbaralhar = document.getElementById("embaralhar");
+	
 		this.eventos();
 
 	}
@@ -45,8 +61,7 @@ class partida {
 				this.socket.emit('truco', "truco");	
 			}else if(pedido == "envido"){
 				this.socket.emit('envido',"envido");
-			}
-			else if(pedido = "flor"){
+			}else if(pedido = "flor"){
 				this.socket.emit('flor',"flor");
 			}else{
 				console.log("Erro! Evento não registrado.");
@@ -58,31 +73,41 @@ class partida {
 		this.socket.on('truco', () =>{
 			window.alert("Truco.");
 		})
-		this.socket.on('envido' () =>{
+		this.socket.on('envido', () =>{
 			window.alert("envido");
 		})
-		this.socket.on('flor' () =>{
+		this.socket.on('flor', () =>{
 			window.alert("flor");
 		})
 
 	}
 	embaralhar(){
-		this.socket.emit('embaralhar', confirma =>{
-			console.log("Embaralhando...");
-			if(confirma){
-				labelEmbaralha.innerHTML = "Pronto";
-				console.log("Pronto");
-			}
+		this.botaoEmbaralhar.addEventListener('click', () =>{
+			this.socket.emit('embaralhar', "embaralha");
+			this.socket.on('embaralhado', confirma => {
+				if(confirma){ //boolean
+					labelEmbaralha.innerHTML = "Pronto";
+					console.log("Pronto");
+				}
+			});
 		})
 	}
 	recebeCartasEmbaralhadas(){
-		this.socket.on('embaralhado', cartas =>{
+		this.socket.on('recebeCartas', cartas =>{
 			this.cartas = cartas;
+			console.log(cartas.jogaUm.cartas.cartaUm);
+			console.log(cartas.jogaUm.cartas.cartaDois);
+			console.log(cartas.jogaUm.cartas.cartaTres+"\n");
+
+			console.log(cartas.jogaDois.cartas.cartaUm);
+			console.log(cartas.jogaDois.cartas.cartaDois);
+			console.log(cartas.jogaDois.cartas.cartaTres);
 		})
 	}
-	/*enviarCarta(){
 
-	}*/
+	enviarCarta(){
+
+	}
 
 }
 part = new partida()
